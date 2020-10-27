d2b69ed232c666ba23504d247fc7e717bba7d6af
Genie <327jhl@naver.com>
2020-10-27 23:37:48 +0900

    #6.7 Poster Component part Two

---
+++ src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  weight: 100vw;
+  display: flex;
+  justify-content: center;
+  font-size: 32px;
+  margin-top: 20px;
+`;
+
+export default () => <Container>L O A D I N G . . .</Container>;
